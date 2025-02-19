94878de3eb99581b5b6a6611802704b0a3ce6c48
lautaronahuelc <lautaronahuel.coria@gmail.com>
2025-02-18 23:22:42 -0300

    Updated_20250218.1

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,12 +1,19 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
 import classes from './page.module.css';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
+
 
   return (
     <>
@@ -25,7 +32,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
